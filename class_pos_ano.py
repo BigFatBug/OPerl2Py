a336114882ef5ef965c91691c24c2a7b669187ad
BigFatBug <253963265@qq.com>
2017-12-10 23:43:07 +0800

    fin

---
+++ class_pos_ano.py
@@ -0,0 +1,137 @@
+# coding=utf8
+import sys
+import HTSeq
+import itertools
+
+class SnpSplitHandler:
+
+    def __init__(self):
+        self.out1 = []
+        self.out2 = []
+        self.vcf_out = []
+
+    def get_out_data(self, spl, index):
+        if index == 1:
+            return '%s\t%s\t%s\t%s\t%s\t%s\thahaha' % (spl[0], spl[1], spl[2], spl[3], spl[4], spl[5])
+        if index == 2:
+            return '%s\t%s\t%s\t%s\t%s\t%s' % (spl[0], spl[1], spl[2], spl[3], spl[4], spl[5])
+        if index == 3:
+            return '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\thahaha' % (spl[0], spl[1], spl[2], spl[3], spl[4], spl[5], spl[7], spl[8], spl[9], spl[10])
+        if index == 4:
+            return '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % (spl[0], spl[1], spl[2], spl[3], spl[4], spl[5], spl[7], spl[8], spl[9], spl[10])
+
+    def snp1(self, datas):
+        for data in datas:
+            spl = data.split('\t')
+            if len(spl[2]) != len(spl[3]):
+                if len(spl) <= 10:
+                    if spl[-1] == 'hahaha':
+                        self.out1.append(self.get_out_data(spl, 1))
+                    else:
+                        self.out1.append(self.get_out_data(spl, 2))
+                else:
+                    if spl[-1] == 'hahaha':
+                        self.out1.append(self.get_out_data(spl, 3))
+                    else:
+                        self.out1.append(self.get_out_data(spl, 4))
+            else:
+                san = 0
+                for i in range(0, len(spl[2])):
+                    san += 1 if spl[2][i] == spl[3][i] else 0
+                if 2 * san >= len(spl[2]):
+                    tot = 0
+                    pot = {}
+                    bsr = {}
+                    bsa = {}
+                    rba = {}
+                    cnt = {}
+
+                    for i in range(0, len(spl[2])):
+                        rba[int(spl[1]) + i] = spl[2][i]
+                    for i in range(0, len(spl[2])):
+                        if spl[2][i] != spl[3][i]:
+                            tot += 1
+                            pot[tot] = int(spl[1]) + i
+                            bsr[tot] = spl[2][i]
+                            bsa[tot] = spl[3][i]
+                    for i in range(1, tot):
+                        if pot[i+1] - pot[i] < 3:
+                            cnt[i] = i + 1
+                    for i in range(1, tot):
+                        if cnt.get(i):
+                            ano = cnt[i]
+                            while cnt.get(ano):
+                                cnt[i] = cnt[ano]
+                                mid = ano
+                                ano = cnt[ano]
+                                cnt.pop(mid)
+                    for i in range(1, tot):
+                        if cnt.get(i):
+                            rsq = bsr[i]
+                            asq = bsa[i]
+                            for m in range(i+1, cnt[i]+1):
+                                for n in range(pot[m-1] + 1, pot[m]):
+                                    rsq += rba[n]
+                                    asq += rba[n]
+                                rsq += bsr[m]
+                                asq += bsa[m]
+                                pot.pop(m)
+                            bsr[i] = rsq
+                            bsa[i] = asq
+                    for i in range(1, tot+1):
+                        if pot.get(i):
+                            if len(spl) <= 10:
+                                self.out1.append('%s\t%s\t%s\t%s\t%s\t%s' % (spl[0], pot[i], bsr[i], bsa[i], spl[4], spl[5]))
+                            else:
+                                self.out1.append('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % (spl[0], pot[i], bsr[i], bsa[i], spl[4], spl[5], spl[7], spl[8], spl[9], spl[10]))
+
+                else:
+                    if len(spl) <= 10:
+                        if spl[-1] == 'hahaha':
+                            self.out1.append(self.get_out_data(spl, 1))
+                        else:
+                            self.out1.append(self.get_out_data(spl, 2))
+                    else:
+                        if spl[-1] == 'hahaha':
+                            self.out1.append(self.get_out_data(spl, 3))
+                        else:
+                            self.out1.append(self.get_out_data(spl, 4))
+
+    def snp2(self, datas):
+        con = {}
+        tel = {}
+        cal = {}
+        tim = {}
+
+        for data in datas:
+            spl = data.split('\t')
+            con[spl[0]][spl[1]][spl[2]][spl[3]] = con.setdefault(spl[0], {}).setdefault(spl[1], {}).setdefault(spl[2], {}).setdefault(spl[3], 0) + 1
+            if spl[-1] == 'hahaha':
+                tel.setdefault(spl[0], {}).setdefault(spl[1], {}).setdefault(spl[2], {})[spl[3]] = 1
+
+        for data in datas:
+            spl = data.split('\t')
+            if con[spl[0]][spl[1]][spl[2]][spl[3]] > 1:
+                if tel.get(spl[0], {}).get(spl[1], {}).get(spl[2], {}).get(spl[3]):
+                    if spl[-1] == 'hahaha':
+                        self.out2.append(data.replace('\thahaha', ''))
+                    else:
+                        continue
+                else:
+                    if tim.get(spl[0], {}).get(spl[1], {}).get(spl[2], {}).get(spl[3]):
+                        mid = cal[spl[0]][spl[1]][spl[2]][spl[3]].split('-')
+                        spl[5] = str(int(spl[5]) + int(mid[0]))
+                        spl[8] = str(int(spl[8]) + int(mid[1]))
+                        spl[9] = str(int(spl[9]) + int(mid[2]))
+                        self.out2.append('\t'.join(spl))
+                    else:
+                        cal.setdefault(spl[0], {}).setdefault(spl[1], {}).setdefault(spl[2], {})[spl[3]] = '%s-%s-%s' % (spl[5], spl[8], spl[9])
+                        tim.setdefault(spl[0], {}).setdefault(spl[1], {}).setdefault(spl[2], {})[spl[3]] = 1
+            else:
+                self.out2.append(data)
+
+    def transfer_vcf(self, datas):
+        for data in datas:
+            d = data.split('\t')
+            info = 'DP=%s;AO=%s;FS=%s;RS=%s;SAF=%s;SAR=%s;HA=%s' % (d[4], d[5], d[6], d[7], d[8], d[9], 1 if d[-1] == 'hahaha' else 0)
+            self.vcf_out.append('\t'.join([d[0], d[1], '.', d[2], d[3], '.', '.', info]))
--- test/test_all.py
+++ test/test_all.py
@@ -3,66 +3,104 @@
 from frb_res_fil import FrbResHandler
 from com_mer_str import ComMerStrHandler
 from snp_split import SnpSplitHandler
+from class_pos_ano import ClassPosAnoHandler
 
 def test_all():
 
-    vcf = '161125_IonXpress_077.freebayes.vcf'
+    ori_vcf = '161125_IonXpress_077.freebayes.vcf'
     sam = '161125_IonXpress_077.bwa.quafil.idf.eff.sam'
     bed = 'MP33_lib.bed'
     fa = 'MP33_seq.fa'
+    xls = 'mp33_pos_rev_foruse.xls'
 
     frb_handler = FrbResHandler()
     com_handler = ComMerStrHandler(bed, fa, sam)
     snp_handler = SnpSplitHandler()
+    ano_handler = ClassPosAnoHandler(fa, xls)
 
-    datas = frb_handler.read_file(vcf)
-    frb_handler.read_title(vcf)
-    frb_handler.exectube(datas)
+    # # frb
+    # read title
+    frb_handler.read_title(ori_vcf)
 
-    frb_handler.write_res_file('temp/file1')
+    # exectube
+    frb_handler.exectube(frb_handler.read_file(ori_vcf))
 
-    frb_handler.write_vcf_out_file('temp/file2')
+    # write file
+    frb_handler.write_res_file('temp/out.freebayes.fil.res')
+    frb_handler.write_vcf_out_file('temp/out.freebayes.fil.vcf')
+
+    # # com
+    # read title
+    com_handler.read_title('temp/out.freebayes.fil.vcf')
 
     # com1_2
-    com_handler.com1(com_handler.read_vcf('temp/file2'))
+    com_handler.com1(com_handler.read_vcf('temp/out.freebayes.fil.vcf'))
     # com2_1
     com_handler.com2(com_handler.out1)
-    # # com3_2
+    # com3_2
     com_handler.com3(com_handler.out2)
-    # # com replace
+    # com replace
     com_handler.com4(com_handler.out3, frb_handler.res_out)
-    # # snp
-    snp_handler.snp1(com_handler.out4)
-    # # snp split
-    snp_handler.snp2(snp_handler.out1)
+    # get vcf out
+    com_handler.filter_vcf(frb_handler.vcf_out_result)
 
-    with open('temp/3', 'w') as a:
+    # write file
+    with open('temp/out.sus.res', 'w') as a:
         for data in com_handler.out1:
             a.write(data + '\n')
-    with open('temp/file4', 'w') as a:
+    with open('temp/out.merge.res', 'w') as a:
         for data in com_handler.out2:
             a.write(data + '\n')
-    with open('temp/file5', 'w') as a:
+    with open('temp/out.check.res', 'w') as a:
         for data in com_handler.out3:
             a.write(data + '\n')
-    with open('temp/file6', 'w') as a:
+    with open('temp/out.mergerev.xls', 'w') as a:
         for data in com_handler.out4:
             a.write(data + '\n')
-    with open('temp/file7', 'w') as a:
+    with open('temp/out.mergerev.vcf', 'w') as a:
+        for title in com_handler.vcf_title:
+            a.write(title)
+        for data in com_handler.vcf_out:
+            a.write(data + '\n')
+
+    # # snp (need com)
+    # read title
+
+    # snp
+    snp_handler.snp1(com_handler.out4)
+    # snp split
+    snp_handler.snp2(snp_handler.out1)
+    # get vcf out
+    snp_handler.transfer_vcf(snp_handler.out2)
+
+    # write file
+    with open('temp/out.mergerev.snpsplit.xls', 'w') as a:
         for data in snp_handler.out1:
             a.write(data + '\n')
-    with open('temp/file8', 'w') as a:
+    with open('temp/out.mergerev.snpsplitrev.xls', 'w') as a:
         for data in snp_handler.out2:
             a.write(data + '\n')
-
-    # com replace filter vcf
-    com_handler.filter_vcf(frb_handler.vcf_out_result)
-    with open('temp/com_replace_out.vcf', 'w') as a:
-        # vcf title
-        for title in frb_handler.vcf_title:
+    with open('temp/out.mergerev.snpsplitrev.vcf', 'w') as a:
+        for title in com_handler.vcf_title:
             a.write(title)
+        for data in snp_handler.vcf_out:
+            a.write(data + '\n')
 
-        for data in com_handler.vcf_out:
+    # # ano
+    # read title
+    ano_handler.read_title('temp/out.mergerev.snpsplitrev.vcf')
+    # class pos ano
+    ano_handler.ano1(ano_handler.read_vcf('temp/out.mergerev.snpsplitrev.vcf'))
+    # get vcf out
+    ano_handler.transfer_vcf(ano_handler.out)
+    # write file
+    with open('temp/out.ano.xls', 'w') as a:
+        for data in ano_handler.out:
+            a.write(data + '\n')
+    with open('temp/out.ano.vcf', 'w') as a:
+        for title in ano_handler.vcf_title:
+            a.write(title)
+        for data in ano_handler.vcf_out:
             a.write(data + '\n')
 
     assert frb_handler.res_out
@@ -74,52 +112,62 @@ def test_all():
     assert com_handler.vcf_out
     assert snp_handler.out1
     assert snp_handler.out2
+    assert snp_handler.vcf_out
+    assert ano_handler.out
+    assert ano_handler.vcf_out
 
 def test1():
-    py_out = open('temp/file1', 'r')
+    py_out = open('temp/out.freebayes.fil.res', 'r')
     perl_out = open('perl_out/1.freebayes.fil.res', 'r')
     assert perl_out.readlines() == py_out.readlines()
     py_out.close()
     perl_out.close()
 
 def test2():
-    py_out = open('temp/file3', 'r')
+    py_out = open('temp/out.sus.res', 'r')
     perl_out = open('perl_out/2.sus.res', 'r')
     assert perl_out.readlines() == py_out.readlines()
     py_out.close()
     perl_out.close()
 
 def test3():
-    py_out = open('temp/file4', 'r')
+    py_out = open('temp/out.merge.res', 'r')
     perl_out = open('perl_out/3.merge.res', 'r')
     assert perl_out.readlines() == py_out.readlines()
     py_out.close()
     perl_out.close()
 
 def test4():
-    py_out = open('temp/file5', 'r')
+    py_out = open('temp/out.check.res', 'r')
     perl_out = open('perl_out/4.check.res', 'r')
     assert perl_out.readlines() == py_out.readlines()
     py_out.close()
     perl_out.close()
 
 def test5():
-    py_out = open('temp/file6', 'r')
+    py_out = open('temp/out.mergerev.xls', 'r')
     perl_out = open('perl_out/5.mergerev.xls', 'r')
     assert perl_out.readlines() == py_out.readlines()
     py_out.close()
     perl_out.close()
 
 def test6():
-    py_out = open('temp/file7', 'r')
+    py_out = open('temp/out.mergerev.snpsplit.xls', 'r')
     perl_out = open('perl_out/6.mergerev.snpsplit.xls', 'r')
     assert perl_out.readlines() == py_out.readlines()
     py_out.close()
     perl_out.close()
 
 def test7():
-    py_out = open('temp/file8', 'r')
+    py_out = open('temp/out.mergerev.snpsplitrev.xls', 'r')
     perl_out = open('perl_out/7.mergerev.snpsplitrev.xls', 'r')
+    assert perl_out.readlines() == py_out.readlines()
+    py_out.close()
+    perl_out.close()
+
+def test8():
+    py_out = open('temp/out.ano.xls', 'r')
+    perl_out = open('perl_out/8.ano.xls', 'r')
     assert perl_out.readlines() == py_out.readlines()
     py_out.close()
     perl_out.close()
